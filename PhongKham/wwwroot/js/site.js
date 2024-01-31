b71e8f78abdbaa72ec8134082da95a42e26b66d9
khanh181 <91577673+khanh181@users.noreply.github.com>
2024-01-31 15:09:20 +0700

    fix lỗi báo đỏ link

--- PhongKham/wwwroot/js/site.js
+++ PhongKham/wwwroot/js/site.js
@@ -39,6 +39,7 @@
 			},
 		},
 	}).mount();
+
 	new Splide("#newstype-carousel", {
 		arrows: false,
 		perPage: 4,
@@ -52,6 +53,7 @@
 	}).mount();
 
 	/*index */
+
 	var s1 = document.createElement("script"),
 		s0 = document.getElementsByTagName("script")[0];
 	s1.async = true;
@@ -60,7 +62,7 @@
 	s1.setAttribute("crossorigin", "*");
 	s0.parentNode.insertBefore(s1, s0);
 
-	$(".mrs-slider").slick({
+/*	$(".mrs-slider").slick({
 		dots: !0,
 		dotsClass: "hero-pagination",
 		infinite: !0,
@@ -73,7 +75,7 @@
 		pauseOnFocus: !0,
 		pauseOnHover: !0,
 		arrows: !0,
-	});
+	});*/
 
 	$("#main-menu").smartmenus({
 		subMenusSubOffsetX: 1,
