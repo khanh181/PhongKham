11a9f58d87d347fcf603eb03e2fe5113fba23998
khanh181 <91577673+khanh181@users.noreply.github.com>
2024-02-02 13:52:38 +0700

    chuc nang doi ten sang trangchu

--- PhongKham/Program.cs
+++ PhongKham/Program.cs
@@ -1,33 +1,56 @@
-using Microsoft.AspNetCore.StaticFiles;
+﻿
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using PhongKham.DataBase;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+/*builder.Services.AddDbContext<ApplycationDbContext>(option =>
+    option.UseSqlServer("Server=.;Database=Bulky;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true")
+);
+*/
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler(exceptionHandlerApp =>
+    {
+        exceptionHandlerApp.Run(async context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            // using static System.Net.Mime.MediaTypeNames;
+            context.Response.ContentType = Text.Plain;
+
+            await context.Response.WriteAsync("An exception was thrown.");
+
+            var exceptionHandlerPathFeature =
+                context.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            {
+                await context.Response.WriteAsync(" The file was not found.");
+            }
+
+            if (exceptionHandlerPathFeature?.Path == "/")
+            {
+                await context.Response.WriteAsync(" Page: Home.");
+            }
+        });
+    });
+
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-// Configure static files and MIME types
-var provider = new FileExtensionContentTypeProvider();
-if (!provider.Mappings.ContainsKey(".woff"))
-{
-    provider.Mappings[".woff"] = "font/woff"; // Add or update the MIME type for .woff
-}
-app.UseStaticFiles(new StaticFileOptions
-{
-    ContentTypeProvider = provider
-});
 
 app.UseRouting();
 
@@ -35,6 +58,17 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{path = trangchu}/{id?}");
+/* tạo trang vào default*/
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/trangchu");
+        return;
+    }
+
+    await next();
+});
 
 app.Run();
