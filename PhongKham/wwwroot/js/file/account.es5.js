bdc319fd0eece1aab1b63a92e54cca8f047e73cd
ntnndvn8502 <tnghia8502@gmail.com>
2024-02-03 17:34:50 +0700

    thêm modal quên mật khẩu

--- PhongKham/wwwroot/js/file/account.es5.js
+++ PhongKham/wwwroot/js/file/account.es5.js
@@ -4,4 +4,13 @@ $(document).ready(function () {
     $("#main-menu .menu-line").eq(5).addClass("activem");
     $("#main-menus .menu-line").eq(5).addClass("activem");
 });
+$("#FPForm").submit(function (e) {
+    $("#OTPForm").show();
+    $("#FPForm").hide();
+    e.preventDefault();
+});
+$("#ModalFP").on('show.bs.modal', function (e) {
+    $("#OTPForm").hide();
+    $("#FPForm").show();
+});
 
